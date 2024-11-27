d654dd8b14e061388eb83718677b65c706afff09
IlyaDOC <ilya22@inbox.ru>
2024-11-27 16:41:54 +0300

    added search, changed styles, added container

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -3,13 +3,15 @@ import { CommonModule } from '@angular/common';
 import { LayoutComponent } from './layout/layout.component';
 import {RouterLink, RouterOutlet} from '@angular/router';
 import { SidebarComponent } from './layout/sidebar/sidebar.component';
+import { SearchComponent } from './layout/search/search.component';
 
 
 
 @NgModule({
   declarations: [
     LayoutComponent,
-    SidebarComponent
+    SidebarComponent,
+    SearchComponent
   ],
   imports: [
     CommonModule,
